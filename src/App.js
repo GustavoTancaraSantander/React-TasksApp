27e63f08ba15d51f62e2dfba7f83ce0121cc4d7e
Gustavo DTS <developer.ias.0110@gmail.com>
2021-06-19 15:36:54 -0400

    01_Knowledge Components: Use functions[js-ES6+] & By Class. Note: important opt use modules {Component} in this version isn't need import react by Default & App.css recognized simple-import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -9,17 +9,32 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div id="HiComponent-id">
+        HiComponent : <HiComponent></HiComponent>
+      </div>
+      <ComponentApp></ComponentApp>
     </div>
   );
 }
+
+function HiComponent() {
+  return <div>Hi, welcome App React</div>;
+}
+const ComponentApp = () => <div>this ComponentApp Arrow ES6</div>;
+
+/* import {Component} from 'react'
+
+class AppReact extends Component {
+  render() {
+    return (
+      <div>
+        <ComponentApp></ComponentApp>
+      </div>
+    );
+  }
+}
+
+export default AppReact; */
 
 export default App;
