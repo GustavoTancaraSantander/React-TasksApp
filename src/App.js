e25839b53aeef337996347271907ad34e08c1a56
Gustavo DTS <developer.ias.0110@gmail.com>
2021-06-19 21:37:12 -0400

    02_Know Props & State {Class}. Use method --> this.setState(). Note: need return in render() => added else

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import { Component } from "react";
 function App() {
   return (
     <div className="App">
@@ -11,17 +11,66 @@ function App() {
         </p>
       </header>
       <div id="HiComponent-id">
-        HiComponent : <HiComponent></HiComponent>
+        HiComponent :
+        <br />
+        <HiComponent myProps="P1" subtitle="loremsub1"></HiComponent>
+        <HiComponent myProps="P2"></HiComponent>
+        <HiComponent myProps="P3!"></HiComponent>
       </div>
       <ComponentApp></ComponentApp>
+      <StateComponent myProps="myProps"></StateComponent>
     </div>
   );
 }
 
-function HiComponent() {
-  return <div>Hi, welcome App React</div>;
+function HiComponent(props) {
+  return (
+    <div>
+      Hi, welcome App React {props.myProps} <b>{props.subtitle}</b>
+    </div>
+  );
 }
 const ComponentApp = () => <div>this ComponentApp Arrow ES6</div>;
+
+class StateComponent extends Component {
+  state = {
+    show: false,
+  };
+  toggleShow = () => {
+    this.setState({ show: false });
+  };
+  render() {
+    if (this.state.show) {
+      return (
+        <div className="App-header">
+          <h3> StateComponent - Class</h3>
+          <p>{this.props.myProps}</p>
+          <button onClick={this.toggleShow}>Hide components</button>
+        </div>
+      );
+    } else {
+      return (
+        <div className="App-header">
+          There not elememts here!
+          {/* <button onClick={() => (this.state.show = !this.state.show)}> */}
+          <button onClick={() => this.setState({ show: true })}>
+            Show components
+          </button>
+        </div>
+      );
+    }
+  }
+}
+
+/* class StateShowBtn extends Component {
+  render() {
+    return (
+      <div>
+        <button onClick={() => this.props.show}> Action </button>
+      </div>
+    );
+  }
+} */
 
 /* import {Component} from 'react'
 
